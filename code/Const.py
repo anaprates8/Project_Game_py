4d2d578628680c8d06d71e544599d1fb17c5e1a0
Ana Paula <anapaulaprates8@gmail.com>
2025-10-06 20:46:26 -0300

    feat: Creating asset folder Inserting all assets Creating the menu: backgroun, sound and fonts Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,15 @@
+# C
+COLOR_BLUE = (0, 15, 140)
+COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 0)
+
+#M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/Menu.py
+++ code/Menu.py
@@ -1,10 +1,35 @@
+import pygame
+from code.Const import WIN_WIDTH, COLOR_BLUE, COLOR_WHITE, MENU_OPTION
+
 
 class Menu:
 
     def __init__(self, window):
         self.window = window
-
-
+        self.surf = pygame.image.load('./asset/Menu.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Sky", COLOR_BLUE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooters", COLOR_BLUE, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 190 + 25 * i))
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # close window
+                    quit()  # end pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf = text_font.render(text, True, text_color).convert_alpha()
+        text_rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(text_surf, text_rect)
