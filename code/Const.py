a11d27415f018418a1942be932d70a2859cdcec2
Ana Paula <anapaulaprates8@gmail.com>
2025-10-09 16:44:21 -0300

    feat: implement movement for players and enemies

--- code/Const.py
+++ code/Const.py
@@ -1,3 +1,5 @@
+import pygame
+
 # C
 COLOR_BLUE = (0, 15, 140)
 COLOR_WHITE = (255, 255, 255)
@@ -5,6 +7,7 @@ COLOR_YELLOW = (255, 255, 0)
 
 
 # E
+EVENT_ENEMY = pygame.USEREVENT + 1
 
 ENTITY_SPEED = {
     'Level1': 0,
@@ -14,6 +17,11 @@ ENTITY_SPEED = {
     'Level5': 4,
     'Level6': 5,
     'Level7': 6,
+    'Player1': 3,
+    'Player2': 3,
+    'Enemy1': 2,
+    'Enemy2': 1,
+
 }
 
 #M
@@ -22,6 +30,23 @@ MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
+
+
+# P
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
+
+
+# S
+SPAWN_TIME = 4000
 
 # W
 WIN_WIDTH = 576
